95384a14cd4e342c79dc365db84c7cd910cc4c8b
Volkmar Nissen <volkmar.nissen@gmail.com>
2024-06-10 12:45:52 +0200

    remove tv.sh

--- tv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-export DEBUG="m2mgithubvalidate validate m2mspecification"
-export PR_NUMBER=81
-export GITHUB_TOKEN=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
-npm run build
-node dist/src/validate.js
-
